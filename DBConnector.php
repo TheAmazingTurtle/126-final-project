ac38c4a7ee224c46148d068ef3a82373e2711178
Jas-1005 <jgmagadan@up.edu.ph>
2025-05-24 06:08:10 +0800

    edited database

--- DBConnector.php
+++ DBConnector.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "";
+$dbname = "gitlog_gourmet.sql";
 
 // create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
